af5e1512db81314a8ed6257673fe01727c4d35c0
VonSieger <claasvonderschen@riseup.net>
2018-12-10 20:05:55 +0000

    MainMenu and PopUpMenu with background

---
+++ src/main/java/controllers/MainMenuController.java
@@ -0,0 +1,44 @@
+package main.java.controllers;
+
+/**
+@author VonSieger
+**/
+
+import javafx.scene.control.Button;
+
+public class MainMenuController{
+  private Button bjButton;
+  private Button oabButton;
+  private Button rouletteButton;
+  private Button homeworkButton;
+
+  public void bjButtonClicked(){
+
+  }
+
+  public void bjHelpButtonClicked(){
+
+  }
+
+  public void oabButtonClicked(){
+
+  }
+  public void oabHelpButtonClicked(){
+
+  }
+  public void rouletteButtonClicked(){
+
+  }
+
+  public void rouletteHelpButtonClicked(){
+
+  }
+
+  public void homeworkButtonClicked(){
+
+  }
+
+  public void homeworkHelpButtonClicked(){
+
+  }
+}
