ed2db4632af487537c93a4f40278b7c02b24784f
VonSieger <claasvonderschen@riseup.net>
2018-12-16 22:56:18 +0000

    Fix import of FXML-Tag in popUpMenuController

--- src/main/java/controllers/PopUpMenuController.java
+++ src/main/java/controllers/PopUpMenuController.java
@@ -8,7 +8,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import javafx.fxml.FMXL;
+import javafx.fxml.FXML;
 
 public class PopUpMenuController{
 
