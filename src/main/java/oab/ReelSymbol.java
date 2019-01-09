3c6d86fecf98af05832f87cd241323303f28ec42
tiststorm <32400997+tiststorm@users.noreply.github.com>
2019-01-09 10:36:17 +0100

    Update ReelSymbol.java
    Symbolpunkte eingefügt

--- src/main/java/oab/ReelSymbol.java
+++ src/main/java/oab/ReelSymbol.java
@@ -4,29 +4,33 @@
  *  @author Tim
  */
 
+/*
+ *  Eine Reihe von Konstanten die jeweils ein Symbol darstellt welches auf den Walzen auftauchen kann
+ * 	Ein neues Walzensymbol mit Pfad der Bilddatei und Punkten erstellen (siehe unten)
+ *  @author Tim
+ */
+
 package main.java.oab;
 
 import javafx.scene.image.Image;
 
-public enum ReelSymbol {
-
-	//Werte sind geändert (2,2,2,5,5,10,0,69,1000)
-	AARON(2,new Image("/main/resources/images/1.png")),
-	ANGELINA(2,new Image("/main/resources/images/2.png")),
-	CLAAS(2, new Image("/main/resources/images/3.png")),
-	HANNES(5, new Image("/main/resources/images/4.png")),
-	JOSCHUA(5,new Image("/main/resources/images/5.png")),
-	TIM(10,new Image("/main/resources/images/6.png")),
-	TOBIAS(0,new Image("/main/resources/images/7.png")),
-	TEST(69,new Image("/main/resources/images/8.png")),
-	XENIA(1000,new Image("/main/resources/images/9.png"));
+public enum ReelSymbol {	
+	AARON(5,50,new Image("/main/resources/images/1.png")),
+	ANGELINA(5,50,new Image("/main/resources/images/2.png")),
+	CLAAS(25,100,new Image("/main/resources/images/3.png")),
+	HANNES(5,50,new Image("/main/resources/images/4.png")),
+	JOSCHUA(5,50,new Image("/main/resources/images/5.png")),
+	TIM(10,75,new Image("/main/resources/images/6.png")),
+	TOBIAS(10,75,new Image("/main/resources/images/7.png")),
+	XENIA(5,50,new Image("/main/resources/images/8.png"));
 
 	private final Image img;
-	private final int points;
+	private final double twice, thrice;
 
-	ReelSymbol(int p, Image i){
+	ReelSymbol(double zwei, double drei, Image i){
 		img = i;
-		points = p;
+		twice = zwei;
+		thrice = drei;
 	}
 	/*
 	 *@return Gibt ein Bild in Form eines Image Objekts zurueck
@@ -37,8 +41,12 @@ public enum ReelSymbol {
 	/*
 	 *@return Gibt die Punkte die dem Walzensymbol zugeordnet sind zurueck
 	 */
-	public int getPoints() {
-		return points;
+	public double getTwo() {
+		return twice;
+	}
+	
+	public double getThree(){
+		return thrice;
 	}
 
 }
